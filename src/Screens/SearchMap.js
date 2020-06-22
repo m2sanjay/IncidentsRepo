1a231b37d9cef5017633f9e1107215862eadd2ae
Shashi <shashi073@gmail.com>
2020-06-22 14:36:44 +0100

    UI changes

--- src/Screens/SearchMap.js
+++ src/Screens/SearchMap.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import {StyleSheet,View,Text,Dimensions,
-  TouchableOpacity,Alert,TextInput, ToastAndroid,ScrollView,
-  ActivityIndicator} from 'react-native';
-import MapView, {Marker,Callout,CalloutSubview,ProviderPropType,Circle} from 'react-native-maps';
+import {
+  StyleSheet, View, Text, Dimensions,
+  TouchableOpacity, Alert, TextInput, ToastAndroid, ScrollView,
+  ActivityIndicator
+} from 'react-native';
+import MapView, { Marker, Callout, CalloutSubview, ProviderPropType, Circle } from 'react-native-maps';
 import LocationItem from './LocationItem';
 import { GoogleAutoComplete } from 'react-native-google-autocomplete';
 import Geocoder from 'react-native-geocoding';
@@ -15,12 +17,13 @@ const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 import { Button } from 'react-native-elements';
+import { Table, TableWrapper, Row } from 'react-native-table-component';
 
 //const SPACE = 0.01;
 const Toast = (props) => {
   if (props.visible) {
-      ToastAndroid.showWithGravityAndOffset(props.message, ToastAndroid.LONG, ToastAndroid.TOP, 25, 150,);
-      return null;
+    ToastAndroid.showWithGravityAndOffset(props.message, ToastAndroid.LONG, ToastAndroid.TOP, 25, 150,);
+    return null;
   }
   return null;
 };
@@ -32,7 +35,7 @@ class SearchMap extends React.Component {
     this.state = {
       cnt: 0,
       events: [],
-      error:null,
+      error: null,
       /*markers: [
         {
           coordinate: {
@@ -59,21 +62,34 @@ class SearchMap extends React.Component {
           },
         },
       ],*/
-      markerCoordinate: {latitude:null, longitude:null},
+      markerCoordinate: { latitude: null, longitude: null },
       isMapReady: false,
       textInp: '',
       toasterVisible: false,
       toasterMsg: '',
+      error: null,
+      isLoaded: false,
+      items: [],
+      tableHead: ['Offence Type', 'Count'],
+      widthArr: [150, 60]
     };
-    this.recordEvent=this.recordEvent.bind(this);
-    this.onChangeMarker=this.onChangeMarker.bind(this);
+    this.recordEvent = this.recordEvent.bind(this);
+    this.onChangeMarker = this.onChangeMarker.bind(this);
     this.navigate = this.navigate.bind(this);
     this.navigateToDetails = this.navigateToDetails.bind(this);
     this.updateMarker = this.updateMarker.bind(this);
   }
 
-  componentWillMount(){
+  componentWillMount() {
     Geocoder.init("");
+  }
+
+  componentDidMount() {
+    fetch('http://192.168.1.14:8080/getIncidents')
+      .then(response => response.json())
+      .then(items => {
+        this.setState({ items })
+      })
   }
 
   show() {
@@ -94,7 +110,7 @@ class SearchMap extends React.Component {
   recordEvent(name) {
     return e => {
       if (e.persist) {
-        e.persist(); 
+        e.persist();
       }
       this.setState(prevState => ({
         events: [this.makeEvent(e, name), ...prevState.events.slice(0, 10)],
@@ -103,157 +119,208 @@ class SearchMap extends React.Component {
   }
   onChangeMarker(e) {
     console.log("Changing Marker");
-    this.setState({markerCoordinate: {latitude: e.nativeEvent.coordinate.latitude, longitude: e.nativeEvent.coordinate.longitude}});
-   
+    this.setState({ markerCoordinate: { latitude: e.nativeEvent.coordinate.latitude, longitude: e.nativeEvent.coordinate.longitude } });
+
     console.log("Getting Address Data");
     Geocoder.from(e.nativeEvent.coordinate.latitude, e.nativeEvent.coordinate.longitude)
-		.then(json => {
-        		var addressComponent = json.results[0];//.address_components[0];
-			console.log(addressComponent.formatted_address);
-		})
-		.catch(error => console.warn(error));
+      .then(json => {
+        var addressComponent = json.results[0];//.address_components[0];
+        console.log(addressComponent.formatted_address);
+      })
+      .catch(error => console.warn(error));
   }
-  navigate(){
+  navigate() {
     //this.props.navigateTo('AddIncident', {data: this.state.markerCoordinate});
-	this.props.enableModal('AddIncident', {data: this.state.markerCoordinate});
+    this.props.enableModal('AddIncident', { data: this.state.markerCoordinate });
   }
-  navigateToDetails(details){
+  navigateToDetails(details) {
     this.props.navigateTo('IncidentDetailsScreen', details);
   }
-  updateMarker(selectedLatitude, selectedLongitude){
+  updateMarker(selectedLatitude, selectedLongitude) {
     this.setState({
-      isMapReady:true,
-      markerCoordinate: {latitude: selectedLatitude,longitude:selectedLongitude }
-  })
+      isMapReady: true,
+      markerCoordinate: { latitude: selectedLatitude, longitude: selectedLongitude }
+    })
   }
   geoSuccess = (position) => {
     this.setState({
-        isMapReady:true,
-        markerCoordinate: {latitude: position.coords.latitude,longitude:position.coords.longitude }
+      isMapReady: true,
+      markerCoordinate: { latitude: position.coords.latitude, longitude: position.coords.longitude }
     })
   }
   geoFailure = (err) => {
     this.setState({
-      isMapReady:true,
-      markerCoordinate: {latitude:22.5726,longitude:88.3639 }
-  })
+      isMapReady: true,
+      markerCoordinate: { latitude: 22.5726, longitude: 88.3639 }
+    })
   }
   onMapLayout = () => {
     let geoOptions = {
       enableHighAccuracy: true,
       timeOut: 20000,
       maximumAge: 60 * 60 * 24
-  };
-  this.setState({isMapReady:false});
-  navigator.geolocation.getCurrentPosition( this.geoSuccess, 
-                                          this.geoFailure,
-                                          geoOptions);
+    };
+    this.setState({ isMapReady: false });
+    navigator.geolocation.getCurrentPosition(this.geoSuccess,
+      this.geoFailure,
+      geoOptions);
   }
   render() {
-    
-    
+    //console.log(this.state.items);
+    // const crimeData = this.state.items.forEach((o) => {
+    //     o.latitude, o.longitude, o.offenceName, o.count
+    // });
+    // console.log(crimeData);
+
+    //var that = this;
+    // let tableData = [];
+    // this.state.items.forEach(item => {
+    //   tableData.push([item.offenceName, item.count]);
+    // });
+    // console.log(tableData);
+
+    let tableData = this.state.items.map(record => ([record.offenceName, record.count]));
+    console.log(tableData);
     return (
       <View style={styles.container}>
         {this.state.toasterVisible ?
-            <Toast visible={this.state.toasterVisible} message={this.state.toasterMsg}/>: null 
+          <Toast visible={this.state.toasterVisible} message={this.state.toasterMsg} /> : null
         }
-        <GoogleAutoComplete apiKey="" debounce={500} minLength={4}>
-        {({
-          handleTextChange,
-          locationResults,
-          fetchDetails,
-          isSearching,
-          inputValue,
-          clearSearch
+        <GoogleAutoComplete apiKey="AIzaSyB4OJsFNMQncqptmQ3nAk-mbUNqqcCYYts" debounce={500} minLength={4}>
+          {({
+            handleTextChange,
+            locationResults,
+            fetchDetails,
+            isSearching,
+            inputValue,
+            clearSearch
           }) => (
-            <React.Fragment>
-              <View style={styles.inputWrapper}>
-                <TextInput style={styles.textInput} placeholder="Search a places" onChangeText={handleTextChange} value={inputValue}/>
-                {/* <Button title="Clear" onPress={clearSearch} /> */}
-                <Button  
-                      icon={{ name: "cancel", size: 30, marginTop: 0, color: "#0A121A"}} 
-                      type="clear" 
-                      onPress={clearSearch} />
-              </View>
-              {isSearching && <ActivityIndicator size="large" color="red" />}
-              <ScrollView style={{zIndex: 2 }}>
-              {locationResults.map(el => (
-                <LocationItem 
-                  {...el}
-                  key={el.id}
-                  fetchDetails={fetchDetails}
-                  clearSearch={clearSearch}
-                  updateMarker={this.updateMarker}
-                />
-              ))}
-              </ScrollView>
-            </React.Fragment>
-          )}
+              <React.Fragment>
+                <View style={styles.inputWrapper}>
+                  <TextInput style={styles.textInput} placeholder="Search a places" onChangeText={handleTextChange} value={inputValue} />
+                  {/* <Button title="Clear" onPress={clearSearch} /> */}
+                  <Button
+                    icon={{ name: "cancel", zIndex:2, size: 30, marginTop: -5, color: "#0A121A" }}
+                    type="clear"
+                    onPress={clearSearch} />
+                </View>
+                {isSearching && <ActivityIndicator size="large" color="red" />}
+                <ScrollView style={{ zIndex: 2 }}>
+                  {locationResults.map(el => (
+                    <LocationItem
+                      {...el}
+                      key={el.id}
+                      fetchDetails={fetchDetails}
+                      clearSearch={clearSearch}
+                      updateMarker={this.updateMarker}
+                    />
+                  ))}
+                </ScrollView>
+              </React.Fragment>
+            )}
         </GoogleAutoComplete>
-        <MapView  style={styles.map}
+        <MapView style={styles.map}
           provider='google'
           zoomEnabled={true}
-          region={{latitude:this.state.markerCoordinate.latitude,
-                  longitude:this.state.markerCoordinate.longitude,
-                  longitudeDelta:0.1,
-                  latitudeDelta:0.1}}
+          region={{
+            latitude: this.state.markerCoordinate.latitude,
+            longitude: this.state.markerCoordinate.longitude,
+            longitudeDelta: 0.1,
+            latitudeDelta: 0.1
+          }}
           zoomTapEnabled={false}
           onPress={this.onChangeMarker}
           onLayout={() => this.onMapLayout()}
         >
-          {this.state.isMapReady ==true ? 
-          <Circle center ={this.state.markerCoordinate} radius={5000}/>:null}
-           {this.state.isMapReady ==true ? 
-             <MapView.Marker coordinate={this.state.markerCoordinate} >
+          {/* {this.state.isMapReady ==true ? 
+              <Circle center ={this.state.markerCoordinate} radius={5000}/>:null} */}
+          {this.state.isMapReady == true ?
+            <MapView.Marker coordinate={this.state.markerCoordinate} >
               <Callout onPress={this.navigate} style={styles.plainView}>
-                <View >
+                <View>
                   <Text>Tap to Add an Incident</Text>
                 </View>
               </Callout>
             </MapView.Marker>
-          : null}
-          {this.props.tickerArray.length > 0 ? (
-            this.props.tickerArray.map((o, i) => (
+            : null}
+          {this.state.items.length > 0 ? (
+            this.state.items.map((o, i) => (
               <Marker key={i}
                 ref={ref => {
                   this.marker1 = ref;
                 }}
-                coordinate={o.coordinate}
-                title={o.title}
-                description={o.description}
+                coordinate={{
+                  latitude: o.latitude,
+                  longitude: o.longitude
+                }}
+                title={o.offenceName}
+                description={o.offenceName}
               >
                 <Callout onPress={() => this.navigateToDetails(o)} style={styles.plainView}>
-                  <View >
-                    <Text>{o.title}</Text>
+                  {/* <View >
+                        <Text>{o.title}</Text>
+                      </View> */}
+                  <View style={styles1.container}>
+                    <ScrollView horizontal={true}>
+                      <View>
+                        <Table borderStyle={{ borderWidth: 1, borderColor: '#C1C0B9' }}>
+                          <Row data={this.state.tableHead} widthArr={this.state.widthArr} style={styles1.header} textStyle={styles1.text} />
+                        </Table>
+                        <ScrollView style={styles1.dataWrapper}>
+                          <Table borderStyle={{ borderWidth: 1, borderColor: '#C1C0B9' }}>
+                            {
+                              tableData.map((rowData, index) => (
+                                <Row
+                                  key={index}
+                                  data={rowData}
+                                  widthArr={this.state.widthArr}
+                                  style={[styles1.row, index % 2 && { backgroundColor: '#F7F6E7' }]}
+                                  textStyle={styles1.text}
+                                />
+                              ))
+                            }
+                          </Table>
+                        </ScrollView>
+                      </View>
+                    </ScrollView>
                   </View>
-              </Callout>
-            </Marker>
+                </Callout>
+              </Marker>
             ))
           ) : null}
-         
-        </MapView>  
+
+        </MapView>
         {/* <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            onPress={() => this.show()}
-            style={[styles.bubble, styles.button]}
-          >
-            <Text>Show</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => this.hide()}
-            style={[styles.bubble, styles.button]}
-          >
-            <Text>Hide</Text>
-          </TouchableOpacity>
-        </View> */}
+              <TouchableOpacity
+                onPress={() => this.show()}
+                style={[styles.bubble, styles.button]}
+              >
+                <Text>Show</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => this.hide()}
+                style={[styles.bubble, styles.button]}
+              >
+                <Text>Hide</Text>
+              </TouchableOpacity>
+            </View> */}
       </View>
     );
+
   }
 }
 
 SearchMap.propTypes = {
   provider: ProviderPropType,
 };
+
+const styles1 = StyleSheet.create({
+  container: { flex: 1, padding: 2, paddingTop: 2, backgroundColor: '#fff' },
+  header: { height: 30, backgroundColor: '#537791' },
+  text: { textAlign: 'center', fontWeight: '100' },
+  dataWrapper: { marginTop: -1 },
+  row: { height: 20, backgroundColor: '#E7E6E1' }
+});
 
 const styles = StyleSheet.create({
   customView: {
@@ -310,7 +377,7 @@ const styles = StyleSheet.create({
   },
   inputWrapper: {
     flexDirection: 'row',
-    zIndex:1,
+    zIndex: 1,
     padding: 10,
     paddingBottom: 0
   },
